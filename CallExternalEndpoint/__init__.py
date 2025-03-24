3f95ba234dc35ec1a0761c9b1385dbe474deeb31
gds-dev-ashu <ashutosh.verma1@gds.ey.com>
2025-03-24 17:59:44 +0530

    CRUDS Completed

--- CallExternalEndpoint/__init__.py
+++ CallExternalEndpoint/__init__.py
@@ -4,7 +4,7 @@
 
 import logging
 import azure.functions as func
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 # Database configuration (replace with your actual database URI)
 from flask_sqlalchemy import SQLAlchemy
@@ -27,58 +27,88 @@ class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(20), unique=False, nullable=False)
     last_name = db.Column(db.String(20), unique=False, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
     age = db.Column(db.Integer, nullable=False)
 
-    # repr method represents how one object of this datatable
+    # This method represents how one object of this datatable
     # will look like
-    def __repr__(self):
-        return f"Name : {self.first_name}, Age: {self.age}"
+    def to_dict(self):
+        """
+        Parse Dict to serializable format.
+        """
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "age": self.age,
+        }
 
 
 @app.route("/api/CallExternalEndpoint", methods=["GET"])
 def call_external_endpoint():
-    """The function"""
-    try:
-        profiles = Profile.query.all()
-        return jsonify(users=profiles)
-    except IntegrityError as ie:
-        # Handle exceptions related to integrity constraints, like unique constraints
-        logging.error("Integrity error occurred: %s", ie)
-        return jsonify(msg="Integrity error"), 400
-
-    except OperationalError as oe:
-        # Handle operational errors, like issues connecting to the database
-        logging.error("Operational error occurred: %s", oe)
-        return jsonify(msg="Operational error"), 500
-
-    except SQLAlchemyError as sa_err:
-        # Catch-all for other SQLAlchemy-related exceptions
-        logging.error("Database error occurred: %s", sa_err)
-        return jsonify(msg="Database error"), 500
+    """
+    # Fetch Profile from db
+    """
+    profiles = Profile.query.all()
+    profiles_list = [profile.to_dict() for profile in profiles]
+    return jsonify(profiles_list)
 
 
+@app.route("/api/CallExternalEndpoint", methods=["POST"])
 def profile():
     """
     # Add Profile to db
     """
-    first_name = "first_name"
-    last_name = "last_name"
-    age = "age"
+    data = request.get_json()
+    first_name = data["first_name"]
+    last_name = data["last_name"]
+    age = data["age"]
+    email = data["email"]
 
-    if first_name != "" and last_name != "" and age is not None:
-        p = Profile(first_name=first_name, last_name=last_name, age=age)
-        db.session.add(p)
+    try:
+        if first_name != "" and last_name != "" and age is not None and email != "":
+            p = Profile(
+                first_name=first_name, last_name=last_name, age=age, email=email
+            )
+            db.session.add(p)
+            db.session.commit()
+            return jsonify(msg="Operation completed"), 200
+        else:
+            return jsonify(msg="Operation failed"), 400
+
+    except:
+        return jsonify(msg="Operation failed"), 500
+
+
+@app.route("/api/CallExternalEndpoint", methods=["PUT"])
+def update():
+    """
+    # Update Profile to db
+    """
+    data = request.get_json()
+    profile_id = data["id"]
+    age = data["age"]
+    email = data["email"]
+
+    if email != "" and age is not None:
+        p = Profile.query.filter_by(id=profile_id).first()
+        p.email = email
+        p.age = age
         db.session.commit()
         return jsonify(msg="Operation completed"), 200
     else:
         return jsonify(msg="Operation failed"), 400
 
 
+@app.route("/api/CallExternalEndpoint", methods=["DELETE"])
 def erase():
     """
     # Delete Profile to db
     """
-    data = Profile.query.get(1)
+    data = request.get_json()
+    profile_id = data["id"]
+    data = Profile.query.get(profile_id)
     db.session.delete(data)
     db.session.commit()
     return jsonify(msg="Operation completed"), 200
@@ -89,5 +119,6 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     # Delegate the request handling to Flask
     """
     with app.app_context():  # Needed for DB operations
+        # db.drop_all()  # Creates the database and tables
         db.create_all()  # Creates the database and tables
     return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
